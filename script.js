89205ab188980febc5d5c401e1b512ba28b5c48d
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2025-01-04 10:08:20 -0600

    uses the ternary operator to establish a condition

--- script.js
+++ script.js
@@ -72,5 +72,7 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 for (const movement of movements) {
-  console.log(movement);
+  movement > 0
+    ? console.log(`You deposited ${movement}`)
+    : console.log(`You withdrew ${Math.abs(movement)}`);
 }
