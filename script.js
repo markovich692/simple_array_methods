8b175c0463d34af5fbd298d0c8a1e7cb17eb7ea9
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2025-01-04 12:32:21 -0600

    uses the .forEach method to loop over the currencies map

--- script.js
+++ script.js
@@ -63,24 +63,28 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 // LECTURES
 
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+//   movement > 0
+//     ? console.log(`You deposited ${movement}`)
+//     : console.log(`You withdrew ${Math.abs(movement)}`);
+// }
+
+// console.log('--FOR-EACH--');
+
+// movements.forEach(function (movement, index) {
+//   movement > 0
+//     ? console.log(`Movement ${index + 1}: You deposited ${movement}`)
+//     : console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
+// });
+
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-for (const movement of movements) {
-  movement > 0
-    ? console.log(`You deposited ${movement}`)
-    : console.log(`You withdrew ${Math.abs(movement)}`);
-}
-
-console.log('--FOR-EACH--');
-
-movements.forEach(function (movement, index) {
-  movement > 0
-    ? console.log(`Movement ${index + 1}: You deposited ${movement}`)
-    : console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
 });
