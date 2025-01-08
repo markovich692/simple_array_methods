5fb36ed6e1b2e546ca80127812903e3660c1349f
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2025-01-08 11:04:47 -0600

    removes credentials from the form input after we log in

--- script.js
+++ script.js
@@ -174,6 +174,9 @@ btnLogin.addEventListener('click', function (event) {
   // console.log(currentAccount);
 
   if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    inputLoginUsername.value = '';
+    inputLoginPin.value = '';
+
     //Display Welcome message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
