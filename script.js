d4095c231630e21b189dca42975cb699a6262c8b
Marc Diallo <mark.devguru@outlook.com>
2025-01-13 10:37:21 -0600

    gets the index of the swimming activity from the uniqueActivities array

--- script.js
+++ script.js
@@ -875,7 +875,8 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // 3. Create an array "allActivities" of all the activities of all the dog breeds
 // 4. Create an array "uniqueActivities" that contains only the unique activities (no activity repetitions).
 // HINT: Use a technique with a special data structure that we studied a few sections ago.
-// 5. Many dog breeds like to swim. What other activities do these dogs like? Store all the OTHER activities these breeds like to do, in a unique array called "swimmingAdjacent".
+// 5. Many dog breeds like to swim. What other activities do these dogs like?
+//  Store all the OTHER activities these breeds like to do, in a unique array called "swimmingAdjacent".
 // 6. Do all the breeds have an average weight of 10kg or more? Log to the console whether "true" or "false".
 // 7. Are there any breeds that are "active"? "Active" means that the dog has 3 or more activities. Log to the console whether "true" or "false".
 // BONUS: What's the average weight of the heaviest breed that likes to fetch? HINT: Use the "Math.max" method along with the ... operator.
@@ -940,3 +941,8 @@ console.log(allActivities);
 const uniqueActivities = [...new Set(allActivities)];
 
 console.log(uniqueActivities);
+
+// Store other activities in an array called "swimmingAdjacent"
+const swimmingIndex = uniqueActivities.findIndex(function (cur, i, arr) {
+  return cur === 'swimming';
+});
