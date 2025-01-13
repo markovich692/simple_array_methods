930e992439a2d2fdd718d5fd3b3bf3e21eaf4813
Marc Diallo <mark.devguru@outlook.com>
2025-01-13 10:02:49 -0600

    creates an array containing the unique activities of the dog breed

--- script.js
+++ script.js
@@ -873,7 +873,8 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // 1. Store the the average weight of a "Husky" in a variable "huskyWeight"
 // 2. Find the name of the only breed that likes both "running" and "fetch" ("dogBothActivities" variable)
 // 3. Create an array "allActivities" of all the activities of all the dog breeds
-// 4. Create an array "uniqueActivities" that contains only the unique activities (no activity repetitions). HINT: Use a technique with a special data structure that we studied a few sections ago.
+// 4. Create an array "uniqueActivities" that contains only the unique activities (no activity repetitions).
+// HINT: Use a technique with a special data structure that we studied a few sections ago.
 // 5. Many dog breeds like to swim. What other activities do these dogs like? Store all the OTHER activities these breeds like to do, in a unique array called "swimmingAdjacent".
 // 6. Do all the breeds have an average weight of 10kg or more? Log to the console whether "true" or "false".
 // 7. Are there any breeds that are "active"? "Active" means that the dog has 3 or more activities. Log to the console whether "true" or "false".
@@ -930,6 +931,12 @@ const dogBothActivities = breeds.find(function (dog) {
 
 console.log(dogBothActivities);
 
+//Create an array of all activities of all the dog breeds
 const allActivities = breeds.flatMap(dog => dog.activities);
 
 console.log(allActivities);
+
+//Create an array containing the unique activities of the dog breed
+const uniqueActivities = [...new Set(allActivities)];
+
+console.log(uniqueActivities);
