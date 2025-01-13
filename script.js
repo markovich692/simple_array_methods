b46d2cf423bf53f79fe40f372bcfd3ed6885bfd5
Marc Diallo <mark.devguru@outlook.com>
2025-01-12 20:08:26 -0600

    inserts challenge 4/5

--- script.js
+++ script.js
@@ -838,12 +838,83 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // console.dir(Array);
 
 //Generates an array of  a hundred random dice roll
-const diceRollHundred = Array.from({ length: 100 }, function (_, index) {
-  const diceRoll = Math.round(Math.random() * 6) + 1;
+// const diceRollHundred = Array.from({ length: 100 }, function (_, index) {
+//   const diceRoll = Math.round(Math.random() * 6) + 1;
 
-  return diceRoll;
-});
+//   return diceRoll;
+// });
 
-console.log(diceRollHundred);
+// console.log(diceRollHundred);
 
 // console.log(diceRoll);
+
+// const movementUI = Array.from(
+//   document.querySelectorAll('.movements__value'),
+//   el => el.textContent.replace('€', '')
+// );
+
+// console.log(movementUI);
+
+// console.log(movements);
+
+// console.log(movements);
+
+// const newMovements = movements.with(2, 2000);
+
+// console.log(movements);
+
+// console.log(newMovements);
+
+// Coding Challenge #4
+
+// This time, Julia and Kate are studying the activity levels of different dog breeds.
+
+// YOUR TASKS:
+// 1. Store the the average weight of a "Husky" in a variable "huskyWeight"
+// 2. Find the name of the only breed that likes both "running" and "fetch" ("dogBothActivities" variable)
+// 3. Create an array "allActivities" of all the activities of all the dog breeds
+// 4. Create an array "uniqueActivities" that contains only the unique activities (no activity repetitions). HINT: Use a technique with a special data structure that we studied a few sections ago.
+// 5. Many dog breeds like to swim. What other activities do these dogs like? Store all the OTHER activities these breeds like to do, in a unique array called "swimmingAdjacent".
+// 6. Do all the breeds have an average weight of 10kg or more? Log to the console whether "true" or "false".
+// 7. Are there any breeds that are "active"? "Active" means that the dog has 3 or more activities. Log to the console whether "true" or "false".
+
+// BONUS: What's the average weight of the heaviest breed that likes to fetch? HINT: Use the "Math.max" method along with the ... operator.
+
+// TEST DATA:
+// const breeds = [
+//   {
+//     breed: 'German Shepherd',
+//     averageWeight: 32,
+//     activities: ['fetch', 'swimming'],
+//   },
+//   {
+//     breed: 'Dalmatian',
+//     averageWeight: 24,
+//     activities: ['running', 'fetch', 'agility'],
+//   },
+//   {
+//     breed: 'Labrador',
+//     averageWeight: 28,
+//     activities: ['swimming', 'fetch'],
+//   },
+//   {
+//     breed: 'Beagle',
+//     averageWeight: 12,
+//     activities: ['digging', 'fetch'],
+//   },
+//   {
+//     breed: 'Husky',
+//     averageWeight: 26,
+//     activities: ['running', 'agility', 'swimming'],
+//   },
+//   {
+//     breed: 'Bulldog',
+//     averageWeight: 36,
+//     activities: ['sleeping'],
+//   },
+//   {
+//     breed: 'Poodle',
+//     averageWeight: 18,
+//     activities: ['agility', 'fetch'],
+//   },
+// ];
